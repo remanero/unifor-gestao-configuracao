35246cb3068785c203de8fab8fdbd6a2f9076150
Renato Mendes Home <remanero@gmail.com>
2015-06-10 00:03:23 -0300

    Fechamento da Issue closes #34
    fechando a ISSUE com o commit

--- MobileMedia05_OO/src/lancs/mobilemedia/sms/NetworkScreen.java
+++ MobileMedia05_OO/src/lancs/mobilemedia/sms/NetworkScreen.java
@@ -7,7 +7,7 @@ import javax.microedition.lcdui.Item;
 import javax.microedition.lcdui.TextField;
 
 /**
- * @author tyoung
+ * @author tyoung - Renato Mendes
  *
  * To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Generation - Code and Comments
